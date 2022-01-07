a8786dfbf37e99479a3c1b10c18a3c4b9efe92d8
Jim Kim <jim.hyunjin.kim@gmail.com>
2022-01-07 14:42:28 -0500

    fetch movie details and paint on movie detail page

--- src/routes/Details.js
+++ src/routes/Details.js
@@ -1,5 +1,32 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react/cjs/react.development";
+
 function Detail() {
-    return <h1>Detail</h1>;
+  const [detail, setDetail] = useState("");
+  const { id } = useParams();
+  const getMovieDetail = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setDetail(json.data.movie);
+    console.log(detail);
+  };
+
+  useEffect(() => {
+    getMovieDetail();
+  }, []);
+
+  return (
+    <div>
+      <h1>Detail</h1>
+      <img src={detail.medium_cover_image} alt={detail.title} />
+      <h2>{detail.title_long}</h2>
+      <p>{detail.description_full}</p>
+      <p>Rating: {detail.rating}</p>
+      <p>Runtime: {detail.runtime} minutes</p>
+
+    </div>
+  );
 }
 
 export default Detail;
